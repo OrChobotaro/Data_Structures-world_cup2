383388bfcab55958781618c791d608a44ad06bcd
or <orch81@gmail.com>
2022-12-20 16:29:03 +0200

    insert and rehash

--- HashPlayerData.h
+++ HashPlayerData.h
@@ -4,11 +4,20 @@
 #include "UnionFindData.h"
 
 class HashPlayerData {
+public:
+    HashPlayerData(int playerID, PlayerData* ptrPlayerUFNode);
+    HashPlayerData(const HashPlayerData& otherData) = default;
+    HashPlayerData& operator=(const HashPlayerData& otherData) = default;
+    ~HashPlayerData() = default;
+
+    int getID() const;
+    PlayerData* getPtr() const;
+
 
 private:
     int m_playerID;
     // pointer to player's pointer in the reversed tree
-    PlayerData* m_ptrPlayerData;
+    PlayerData* m_ptrPlayerUFNode;
 };
 
 
